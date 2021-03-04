a34633fc93db2cd74e560f395116356ebd6783ec
Robert Zdeb <robinio123@gmail.com>
2021-03-05 00:35:10 +0100

    Use @Transactional annotation to handle book saving

--- src/main/java/pl/jobostart/app/dao/BookDaoImpl.java
+++ src/main/java/pl/jobostart/app/dao/BookDaoImpl.java
@@ -4,32 +4,24 @@ import org.springframework.stereotype.Repository;
 import pl.jobostart.app.model.Book;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 @Repository
+@Transactional
 public class BookDaoImpl implements BookDao {
 
-    @PersistenceUnit
-    EntityManagerFactory emFactory;
+    @PersistenceContext
+    EntityManager entityManager;
 
     @Override
     public void save(Book book) {
-        EntityManager entityManager = emFactory.createEntityManager();
-        EntityTransaction tx = entityManager.getTransaction();
-        tx.begin();
         entityManager.persist(book);
-        tx.commit();
-        entityManager.close();
-
     }
 
     @Override
     public Book get(Long id) {
-        EntityManager entityManager = emFactory.createEntityManager();
         Book book = entityManager.find(Book.class, id);
-        entityManager.close();
         return book;
     }
 }
